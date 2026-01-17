704a7e343c32e510a902a365dafa561cc3212e95
Rafael <rafaelschmi@gmail.com>
2026-01-17 18:53:08 +0800

    chatbot

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
-    path('', views.chat_home, name='chat_home'),
+    path('chat/', views.chat_home, name='chat_home'),
     path('chat/send/', views.send_message, name='send_message'),
     path('chat/clear/', views.clear_chat, name='clear_chat'),
 ]
