ae2e0ae9e66142d5bb3f2bea2e944c72be66b448
Rafael <rafaelschmi@gmail.com>
2026-01-17 17:58:52 +0800

    chatbot

--- wellness_centre/urls.py
+++ wellness_centre/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
     path('chatbot/', include('chatbot.urls')),
 ]
 
